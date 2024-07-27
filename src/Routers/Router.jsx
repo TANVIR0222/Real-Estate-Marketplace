23e91005e555ba518a18da98e092f24d42db8044
TANVIR0222 <tanvirislam3912@gmail.com>
2024-07-27 14:19:24 +0600

    create pages and routes

---
+++ src/Routers/Router.jsx
@@ -0,0 +1,38 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Main/Main";
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import SingUp from "../pages/SingUp";
+import Singin from "../pages/Singin";
+import About from "../pages/About";
+  
+ export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>,
+      children:[
+        {
+            path: "/",
+            element:<Home></Home>
+        },
+        {
+            path: "/profile",
+            element:<Profile></Profile>
+        },
+        {
+            path: "/singup",
+            element:<SingUp></SingUp>
+        },
+        {
+            path: "/singin",
+            element:<Singin></Singin>
+        },
+        {
+            path: "/about",
+            element:<About></About>
+        },
+      ]
+    },
+  ]);
---
+++ src/pages/Singin.jsx
@@ -0,0 +1,10 @@
+
+const Singin = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Singin;
