61eb240d2c4c2ed9ab1b8d8114633fa66b0ea969
TANVIR0222 <tanvirislam3912@gmail.com>
2024-07-31 15:00:52 +0600

    complete image upload functionality

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -2,17 +2,28 @@ import { Button, Label, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { app } from "../firebase/firebase.config";
-import {  getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 const UserProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
   const fileRef = useRef(null);
-
   const [file, setFile] = useState(undefined);
   //   file uploade see profile
   const [fileuplode, setFileuplode] = useState(0);
-  console.log(fileuplode);
-  console.log(file);
+
+  // error
+  const [fileUploadeError, setFileUploadeError] = useState(false);
+
+  // frome data
+  const [formData, setFormData] = useState({});
+
+  // console.log(formData);
+  // console.log(file);
 
   useEffect(() => {
     if (file) {
@@ -26,13 +37,25 @@ const UserProfile = () => {
     const filename = new Date().getTime() + file.name;
     const storeRef = ref(storsge, filename);
     const uploadTask = uploadBytesResumable(storeRef, file);
-    uploadTask.on("state_changed",
-       (snapshot) => {
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      // number gula decimel tai take purnnow nuber a rupantor
-      // setFileuplode(Math.round(progress));
-      console.log('uplode ' + progress + '% done');
-    });
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // number gula decimel tai take purnnow nuber a rupantor
+        setFileuplode(Math.round(progress));
+      },
+      // set Error
+      () => {
+        setFileUploadeError(true);
+      },
+      // uplode profile img
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadeURL) => {
+          setFormData({ ...formData, avatar: downloadeURL });
+        });
+      }
+    );
   };
 
   return (
@@ -53,9 +76,25 @@ const UserProfile = () => {
             <img
               onClick={() => fileRef.current.click()}
               className=" w-20 h-20 rounded"
-              src={currentUser.avatar}
+              // current image uploade & prev image
+              src={formData.avatar || currentUser.avatar}
               alt=""
             />
+            <p className="text-sm my-2 self-center">
+              {fileUploadeError ? (
+                <span className="text-red-700">
+                  Error Image Upload (image must be less than 2 md){" "}
+                </span>
+              ) : fileuplode > 0 && fileuplode < 100 ? (
+                <span className="text-slate-700">Uploading{fileuplode}%</span>
+              ) : fileuplode === 100 ? (
+                <span className="text-green-700">
+                  Image Successfully Uploaded!
+                </span>
+              ) : (
+                ""
+              )}
+            </p>
           </div>
 
           <div>
